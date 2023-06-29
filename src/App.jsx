5b1952ddcb62f9117fc30776aecc878189d0dce2
nani-samireddy <nanisamireddy05@gmail.com>
2023-06-29 19:26:31 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,63 @@
+import { useEffect,useState } from 'react';
+import './App.css';
+import faceIO from "@faceio/fiojs";
+
+
+
+function App() {
+ const [faceio, setFaceio] = useState(null);
+
+ useEffect(() => {
+   setFaceio(new faceIO("fioa3854"));
+ }, []);
+
+
+
+const handleSignIn = async () => {
+  try {
+    let response = await faceio.enroll({
+      locale: "auto",
+      payload: {
+        email: "nanisamireddy05@gmail.com",
+        pin: "12345",
+      },
+    });
+
+    console.log(` Unique Facial ID: ${response.facialId}
+      Enrollment Date: ${response.timestamp}
+      Gender: ${response.details.gender}
+      Age Approximation: ${response.details.age}`);
+  } catch (error) {
+    console.log(error);
+  }
+
+  
+};
+
+const handleLogIn = async () => {
+  try {
+    let response = await faceio.authenticate({
+      locale: "auto",
+    });
+
+    console.log(` Unique Facial ID: ${response.facialId}
+          PayLoad: ${response.payload}
+          `);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+
+  return (
+    <>
+      <section>
+        <h1>Face Authentication by FaceIO</h1>
+        <button onClick={handleLogIn}>Log-in</button>
+        <button onClick={handleSignIn}>Sign-Up</button>
+      </section>
+    </>
+  );
+}
+
+export default App
